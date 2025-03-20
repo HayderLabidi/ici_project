7909aa7ba2cacfc8b678fc5eea02fbaaff48ac2e
Hayder Labidi <108490838+HayderLabidi@users.noreply.github.com>
2025-03-20 14:04:52 +0100

    chore: Refactor productAction.js and HomeScreen.js for better code organization and readability

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import { thunk } from 'redux-thunk'; 
-import data from './Data';
+import {createStore,compose,applyMiddleware,combineReducers} from 'redux';
+import {thunk} from 'redux-thunk';
+import { productListReducer } from './reducers/productReducers';
 
 const initialState = {};
-const reducer = (state, action) => {
-    return {
-        products: data.products
-    };
-};
-
+const reducer = combineReducers({
+    productList: productListReducer,
+})
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducer,
+     initialState, 
+     composeEnhancer(applyMiddleware(thunk)))
 
-const store = createStore(reducer,initialState,composeEnhancer(applyMiddleware(thunk)) );
-
-export default store;
+export default store ;
