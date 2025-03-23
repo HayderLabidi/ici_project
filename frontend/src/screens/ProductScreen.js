08300b53dc8453fe810e2e8f75a46dfd4dd2e010
Hayder Labidi <108490838+HayderLabidi@users.noreply.github.com>
2025-03-23 23:13:56 +0100

    chore: Refactor code for better organization and readability

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,84 +1,79 @@
-import React from 'react';
-import data from '../Data';
-import { useParams , Link} from 'react-router-dom';
+import React, { useEffect } from 'react'
+import {useParams, Link} from "react-router-dom"
 import Rating from '../components/Rating';
+import { useDispatch, useSelector } from 'react-redux';
 import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox'; // Add this line to import the MessageBox component
+import MessageBox from '../components/MessageBox';
+import { detailsProduct } from '../actions/productActions';
 
-export default function ProductScreen() {
-    const { id } = useParams();
-    const product = data.products.find((x) => x._id === Number(id) );
-    const loading = false;
-    const error = null;
+export default function ProductScreen(props) {
+ const  dispatch= useDispatch();
+ const productId = props.match.params.id;
+const productDetails = useSelector(state => state.productDetails);
+const {loading, error, product}=productDetails;
+useEffect(() => {
+  dispatch(detailsProduct(productId))
+},[dispatch,productId])
     return (
       <div>
-        { loading ?(
-            <LoadingBox></LoadingBox>
-          )
-          : error ? (
-            <MessageBox variant="danger">{error}</MessageBox>
-          ) : (
-            <div>
-              <Link to='/'>Back to result</Link>
-              <div className="row top">
-                  <div className="col-2">
-                      <img className="large" src={product.image} alt={product.name} />
-                      <div className="col-1">
-                        <ul>
-                          <li>
-                            <h1>{product.name}</h1>
-                          </li>
-                          <li>
-                            <Rating rating={product.rating}></Rating>
-                          </li>
-                          <li>
-                            Price: ${product.price}
-                          </li>
-                          <li>
-                            Category: {product.category}
-                          </li>
-                          <li>
-                            Description: <p>{product.description}</p>
-                          </li>
-                        </ul>
-                      </div>
-                      <div className='col-1'>
-                        <div className='card card-body'>
-                          <ul>
-                            <li>
-                              <div className='row'> 
-                                <div>
-                                  Price:
-                                </div>
-                                <div className='price'>
-                                  ${product.price}
-                                </div>
-                              </div>
-                            </li>
-                            <li>
-                              <div className='row'>
-                                <div>Status: </div>
-                                <div>
-                                  {/* TERNER  === if else */}
-                                  {product.CountInStock > 0 ?(
-                                    <span className='success'>In stock</span>
-                                  ) : (
-                                    <span className='danger'>Unvaialble</span>
-                                  )}
-                                </div>
-                              </div>
-                            </li>
-                            <li>
-                              <button className='primary block'>Add to cart</button>
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
+      { loading ? (
+         <LoadingBox></LoadingBox>
+        ): error ? (
+         <MessageBox variant="danger">{error}</MessageBox>
+        ):(
+          <div>
+     <Link to="/"> Back to result</Link>
+      <div className='row top'>
+        <div className='col-2'>
+         <img className='large' src={product.image} alt={product.name}></img>
+        </div>
+        <div className='col-1'>
+            <ul>
+              <li>
+                <h1>{product.name}</h1>
+              </li>
+              <li>
+                <Rating rating={product.rating}></Rating>
+              </li>
+              <li>Price: ${product.price}</li>
+              <li>Description : <p>{product.description}</p></li>
+            </ul>
+        </div>
+        <div className='col-1'>
+          <div className='card card-body'>
+            <ul>
+              <li>
+                <div className='row'>
+                  <div>Price </div>
+                  <div className='price'>${product.price}</div>
+                </div>
+              </li>
+              <li>
+                <div className='row'>
+                  <div>Status</div>
+                  <div>
+                 {/* TERNAIRE === if else */}
+                    {product.CountInStock>0 ? (
+                      <span className='success'>In Stock</span> 
+                    ):(
+                      <span className='danger'>Unvailable</span>
+                    )
+                   }
                   </div>
-              </div>
-            </div>
-          )
-        }
+                </div>
+              </li>
+              <li>
+                <button className='primary block'>Add to cart</button>
+              </li>
+            </ul>
+          </div>
+          </div>
+        </div>
       </div>
-    );
+        )
+        }      
+  </div>
+  
+
+  )
 }
