4ab517b23799ffa3af4847647585cedd92c80e19
Hayder Labidi <108490838+HayderLabidi@users.noreply.github.com>
2025-04-02 12:27:12 +0200

    chore: Refactor Product component for improved code organization and readability

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,21 +1,20 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants';
-
-export const cartReducer = (state = { cartItems: [] }, action) => {
-  switch (action.type) {
-    case CART_ADD_ITEM:
-      const item = action.payload;
-      const existItem = state.cartItems.find((x) => x.product === item.product);
-      if (existItem) {
-        return {
-          ...state,
-          cartItems: state.cartItems.map((x) =>
-            x.product === existItem.product ? item : x
-          ),
-        };
-      } else {
-        return { ...state, cartItems: [...state.cartItems, item] };
-      }
-    default:
-      return state;
-  }
-};
+import React  from 'react';
+ import {useParams,useLocation} from 'react-router-dom';
+ 
+ export default function CartScreen() {
+     const params = useParams();
+     const productId = params.id;
+     const {search} =useLocation();
+     const qtyInUrl = new URLSearchParams(search).get('qty');
+     const qty = qtyInUrl?Number(qtyInUrl):1;
+     
+    
+     return (
+         <div>
+           <h1>Cart Screen</h1>
+           <p>
+             ADD TO CART : ProductID: {productId} Qty: {qty}
+           </p>
+         </div>
+       );
+     }
