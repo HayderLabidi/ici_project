cee3a54452a1cc0737058b50e41dcfb09c65638f
Hayder Labidi <108490838+HayderLabidi@users.noreply.github.com>
2025-04-10 15:23:43 +0200

    chore: Remove unnecessary whitespace in server.js file

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -5,6 +5,7 @@ import expressAsyncHandler from 'express-async-handler';
 const userRouter = express.Router();
 
 userRouter.get('/seed', expressAsyncHandler ( async (req, res) => {
+    await User.deleteMany();
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdUsers });
   }));
